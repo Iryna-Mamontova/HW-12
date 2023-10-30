5f8a30f19117840e2a6e89017c02cd6dd79ec34a
Iryna_Mamontova <irishaym@gmail.com>
2023-10-30 23:31:28 +0200

    awards added

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ import films_data
 import os
 import string
 import shutil
+import awards
 
-# shutil.rmtree('Harry Potter')
-
-os.mkdir('Harry Potter')
-os.chdir('Harry Potter')
-for film in films_data.films_titles["results"]:
-    f_title = film["title"].replace(': ', '_')
-    os.mkdir(f_title)
-    os.chdir(f_title)
-    for letters in string.ascii_uppercase:
-      os.mkdir(letters)
-    os.chdir('..')
+# os.mkdir('Harry Potter')
+# os.chdir('Harry Potter')
+# for film in films_data.films_titles["results"]:
+#     f_title = film["title"].replace(': ', '_')
+#     os.mkdir(f_title)
+#     os.chdir(f_title)
+#     for letters in string.ascii_uppercase:
+#       os.mkdir(letters)
+#     os.chdir('..')
     
