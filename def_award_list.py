3041f3c6f0618ecc758adddee1c36eb87d029750
Iryna_Mamontova <irishaym@gmail.com>
2023-11-01 00:40:32 +0200

    fix

--- def_award_list.py
+++ def_award_list.py
@@ -1,11 +1,12 @@
 import awards
 def award_list(id):
   list_name = []
-  for film in awards.films_awards[0]['results']:
-    if film['movie']['imdb_id'] == id:
-      list_name.append({'type': film['type'],
-                      'award_name': film['award_name'],
-                      'award': film['award']
+  for film in awards.films_awards:
+    for mv in film['results']:
+      if mv['movie']['imdb_id'] == id:
+        list_name.append({'type': mv['type'],
+                      'award_name': mv['award_name'],
+                      'award': mv['award']
                       })
   list_name = list(sorted(list_name, key = lambda fil_aw: fil_aw['award_name']))
   return list_name
